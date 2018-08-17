48cbe4bec5e3e100e084d5b18c06227f46657753
Andres Reyes <oreyes@cads.com.gt>
2018-08-17 08:51:55 -0600

    Se agrego los componetes de angular material

--- src/app/configuration/material.ts
+++ src/app/configuration/material.ts
@@ -1,8 +1,8 @@
-import {MatCardModule, MatInputModule, MatIconModule, MatButtonModule, MatSlideToggleModule, MatTooltipModule, MatSnackBarModule} from '@angular/material';
+import { MatCardModule, MatInputModule, MatIconModule, MatButtonModule, MatSlideToggleModule, MatTooltipModule, MatSnackBarModule,MatTableModule, MatPaginatorModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatCardModule, MatInputModule, MatIconModule, MatButtonModule, MatSlideToggleModule, MatTooltipModule, MatSnackBarModule],
-  exports: [MatCardModule, MatInputModule, MatIconModule, MatButtonModule, MatSlideToggleModule, MatTooltipModule, MatSnackBarModule],
+  imports: [MatCardModule, MatInputModule, MatIconModule, MatButtonModule, MatSlideToggleModule, MatTooltipModule, MatSnackBarModule,MatTableModule, MatPaginatorModule],
+  exports: [MatCardModule, MatInputModule, MatIconModule, MatButtonModule, MatSlideToggleModule, MatTooltipModule, MatSnackBarModule,MatTableModule, MatPaginatorModule],
 })
 export class MaterialModule { }
