de8987ecbdd6bd18794e547c39998b4a19a98d90
Andres Reyes <oreyes@cads.com.gt>
2018-08-17 08:51:22 -0600

    Se agrego a las rutas el list company y el edit company

--- src/app/configuration/configuration.routes.ts
+++ src/app/configuration/configuration.routes.ts
@@ -2,6 +2,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { StartComponent } from "./start/start.component";
 import { CompanyComponent } from "./company/company.component";
 import { UserComponent } from "./user/user.component";
+import { CompanyListComponent } from "./company-list/company-list.component";
 
 const pagesRoutes: Routes = [
   {
@@ -9,6 +10,8 @@ const pagesRoutes: Routes = [
     children:[
       { path: '', component: StartComponent },
       { path: 'company', component: CompanyComponent },
+      { path: 'company/:id', component: CompanyComponent },
+      { path: 'company-list', component: CompanyListComponent },
       { path: 'user', component: UserComponent }
     ]
   }
